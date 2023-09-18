1cf2f91c54bc8500880b6e9f3487d58b217eb964
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-09-17 22:22:18 -0300

    Initial Setup

---
+++ FinTrac/UnitTests/UnitTest2.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
