a2380c619ba987119658b069238752d1e50622e3
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-09-20 21:01:45 -0300

    [GREEN] Password UpperCase Validator

--- FinTrac/BusinessLogic/User/User.cs
+++ FinTrac/BusinessLogic/User/User.cs
@@ -25,11 +25,27 @@ namespace BusinessLogic.User
         {
             int minLength = 10;
             int maxLength = 30;
+            bool hasUpperCase = false;
 
             if(posiblePassword.Length < minLength || posiblePassword.Length > maxLength)
             {
                 throw new ExceptionValidateUser("ERROR ON PASSWORD");
             }
+
+            for (int i = 0; i < posiblePassword.Length; i++)
+            {
+                if (posiblePassword[i] >= 65 && posiblePassword[i] <= 90)
+                {
+                    hasUpperCase = true;
+                }
+
+            }
+
+            if (!hasUpperCase) 
+            {
+                throw new ExceptionValidateUser("ERROR ON PASSWORD");
+            }
+
             return true;
         }
 
