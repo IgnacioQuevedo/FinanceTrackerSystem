c39b1d3f286775f56bba924b3ac606d1a0a07e9d
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-09-21 01:32:02 -0300

    [REFACTOR] Property LastName Validator

--- FinTrac/BusinessLogic/User/User.cs
+++ FinTrac/BusinessLogic/User/User.cs
@@ -15,7 +15,7 @@ namespace BusinessLogic.User
 
         public string FirstName { get; set; } = "";
 
-        public string LastName { get; set; }
+        public string LastName { get; set; } = "";
         
         public string Password { get; set; } = "";
 
