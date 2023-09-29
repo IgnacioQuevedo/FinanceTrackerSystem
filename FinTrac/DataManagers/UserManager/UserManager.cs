b11ba4ee2b7bbd9205f6053aa8d0d4df8cddabe5
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-09-29 20:48:39 -0300

    [GREEN] Id is generated sucessfully

--- FinTrac/DataManagers/UserManager/UserManager.cs
+++ FinTrac/DataManagers/UserManager/UserManager.cs
@@ -21,6 +21,7 @@ namespace DataManagers.UserManager
             if (ValidateAddUser(user))
             {
                 FormatProperties(user);
+                user.Id = _memoryDatabase.Users.Count + 1;                                                                                                              
                 _memoryDatabase.Users.Add(user);
             }
         }
