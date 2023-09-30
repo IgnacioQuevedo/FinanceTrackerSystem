c278e4f53ca649bb8313af3e0ec052ef8b2bf057
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2023-09-30 18:10:50 -0300

    [RED] Given correct title should return true

---
+++ FinTrac/BusinessLogic/Transaction/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Transaction
+{
+    public class Transaction
+    {
+        public string Title { get; set; }
+
+        public Transaction()
+        {
+        }
+
+        public bool ValidateTitle()
+        {
+            return false;
+        }
+    }
+}
---
+++ FinTrac/ProjectTests/TransactionTests.cs
@@ -0,0 +1,28 @@
+using BusinessLogic;
+using NuGet.Frameworks;
+using System.Runtime.ExceptionServices;
+using BusinessLogic.Transaction;
+
+namespace TestProject1;
+[TestClass]
+public class TransactionTests
+{
+    private Transaction genericTransaction;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        genericTransaction = new Transaction();
+        genericTransaction.Title = "Title";
+    }
+
+    [TestMethod]
+    public void GivenCorrectTitle_ShouldReturnTrue()
+    {
+        Assert.AreEqual(true, genericTransaction.ValidateTitle());
+    }
+
+
+
+
+}
