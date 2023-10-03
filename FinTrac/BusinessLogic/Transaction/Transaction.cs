3adb96d97f4343d4446e2ef935aae6ba6e3a10de
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2023-10-03 16:12:30 -0300

    [REFACTOR] Improved if to make it more readable with bool vars

--- FinTrac/BusinessLogic/Transaction/Transaction.cs
+++ FinTrac/BusinessLogic/Transaction/Transaction.cs
@@ -60,7 +60,9 @@ namespace BusinessLogic.Transaction
 
         public void ValidateAccount()
         {
-            if (Account is CreditCardAccount && Type == TypeEnum.Income)
+            bool typeIsIncome = Type == TypeEnum.Income;
+
+            if (Account is CreditCardAccount && typeIsIncome)
             {
                 throw new ExceptionValidateTransaction("Transaction of type income cant be associated to a credit account");
             }
