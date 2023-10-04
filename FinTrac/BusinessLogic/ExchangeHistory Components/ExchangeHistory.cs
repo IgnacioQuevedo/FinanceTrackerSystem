9c295eb84d4082bb22abd6960b00fe16cfe02e69
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-10-04 17:31:43 -0300

    [RED] Given currency, should be setted

---
+++ FinTrac/BusinessLogic/ExchangeHistory Components/ExchangeHistory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.ExchangeHistory_Components
+{
+    public class ExchangeHistory
+    {
+
+
+    }
+}
---
+++ FinTrac/ProjectTests/ExchangeHistoryTests.cs
@@ -0,0 +1,36 @@
+using BusinessLogic;
+using BusinessLogic.Account_Components;
+using BusinessLogic.ExchangeHistory_Components;
+using BusinessLogic.User_Components;
+using NuGet.Frameworks;
+using System.Runtime.ExceptionServices;
+
+namespace TestProject1;
+[TestClass]
+public class ExchangeHistoryTests
+{
+
+    [TestInitialize]
+
+    public void Initialize()
+    {
+
+
+    }
+
+
+    [TestMethod]
+    public void GivenBadge_ShouldBeSetted()
+    {
+
+        CurrencyEnum currency = CurrencyEnum.USA;
+
+        ExchangeHistory exchangeValue = new ExchangeHistory();
+
+        exchangeValue.Currency = currency;
+
+    }
+
+
+    
+}
