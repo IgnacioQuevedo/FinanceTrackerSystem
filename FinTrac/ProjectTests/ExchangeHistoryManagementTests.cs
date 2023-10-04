2a4cfd6b7da6d97fd3c9cb2f5bf74ab2c3f82f01
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-10-04 20:44:01 -0300

    [REFACTOR] Improved method

--- FinTrac/ProjectTests/ExchangeHistoryManagementTests.cs
+++ FinTrac/ProjectTests/ExchangeHistoryManagementTests.cs
@@ -10,21 +10,40 @@ namespace TestProject1
     [TestClass]
     public class ExchangeHistoryManagementTests
     {
+        #region Initialize
+
+        private string firstName;
+        private string lastName;
+        private string email;
+        private string password;
+        private string address;
+        private User genericUser;
+
+        [TestInitialize]
+        public void Init()
+        {
+            firstName = "Austin";
+            lastName = "Ford";
+            email = "austinFord@gmail.com";
+            password = "AustinF2003";
+            address = "NW 2nd Ave";
+            genericUser = new User(firstName, lastName, email, password, address);
+        }
+
+        #endregion
+
+        #region Add Exchange History
+
         [TestMethod]
         public void GivenAExchangeHistory_ShouldBePossibleToAdd()
         {
             DateTime date = new DateTime(2023 / 10 / 4);
             ExchangeHistory exchangeHistoryExample = new ExchangeHistory(CurrencyEnum.USA, 38.5M, date);
-
-            string firstName = "Austin";
-            string lastName = "Ford";
-            string email = "austinFord@gmail.com";
-            string password = "AustinF2003";
-            string address = "NW 2nd Ave";
-            User genericUser = new User(firstName, lastName, email, password, address);
-
             genericUser.AddExchangeHistory(exchangeHistoryExample);
+
             Assert.AreEqual(exchangeHistoryExample, genericUser.MyExchangeHistory[0]);
         }
+
+        #endregion
     }
 }
