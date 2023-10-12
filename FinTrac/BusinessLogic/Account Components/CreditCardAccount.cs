afd725083141fe6870ef7c564cc7ea22332be447
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-10-12 02:05:04 -0300

    fix: Last4Digits was wrong validated, also added alerts

--- FinTrac/BusinessLogic/Account Components/CreditCardAccount.cs
+++ FinTrac/BusinessLogic/Account Components/CreditCardAccount.cs
@@ -77,7 +77,7 @@ namespace BusinessLogic.Account_Components
         {
             int lengthOfLastDigits = Last4Digits.Length;
 
-            if (string.IsNullOrEmpty(Last4Digits) || lengthOfLastDigits < 4 || !IsNumericChain())
+            if (string.IsNullOrEmpty(Last4Digits) || lengthOfLastDigits != 4 || !IsNumericChain())
             {
                 throw new ExceptionValidateAccount("ERROR ON LAST 4 DIGITS");
             }
