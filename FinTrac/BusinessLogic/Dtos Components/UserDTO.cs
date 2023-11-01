94ac60fc3feaee7864420335778c126edad550d2
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-11-01 17:44:56 -0300

    [RED] Given firstName should be setted

---
+++ FinTrac/BusinessLogic/Dtos Components/UserDTO.cs
@@ -0,0 +1,6 @@
+namespace BusinessLogic.Dtos_Components;
+
+public class UserDTO
+{
+    
+}
--- FinTrac/ProjectTests/Dtos Tests/UserDTO_Tests.cs
+++ FinTrac/ProjectTests/Dtos Tests/UserDTO_Tests.cs
@@ -1,8 +1,14 @@
-namespace BusinessLogicTests.UserDTO_Tests;
-
 public class UserDTO_Tests
 {
-    
-    
-    
+
+    [TestMethod]
+    public void GivenFirstName_ShouldBeSetted()
+    {
+        string firstName = "Ignacio";
+        UserDTO userDTO = new UserDTO();
+        userDTO.FirstName = firstName;
+
+        Assert.AreEqual(firstName,userDTO.FirstName);
+        
+    }
 }
