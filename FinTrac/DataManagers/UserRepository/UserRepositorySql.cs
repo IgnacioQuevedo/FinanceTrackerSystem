1a85bd785c854cead71f146dc47012a45c28c306
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-11-03 00:20:55 -0300

    [GREEN] It is possible to check if email and pswd are correct

--- FinTrac/DataManagers/UserRepository/UserRepositorySql.cs
+++ FinTrac/DataManagers/UserRepository/UserRepositorySql.cs
@@ -2,11 +2,11 @@ using BusinessLogic.User_Components;
 using DataManagers;
 
 namespace DataManagers;
+
 public class UserRepositorySql
 {
-
     private SqlContext _database;
-    
+
     public UserRepositorySql(SqlContext database)
     {
         _database = database;
@@ -18,7 +18,7 @@ public class UserRepositorySql
         _database.Users.Add(userToAdd);
         _database.SaveChanges();
     }
-    
+
     public void EmailUsed(string UserEmail)
     {
         foreach (var someUser in _database.Users)
@@ -30,8 +30,27 @@ public class UserRepositorySql
         }
     }
 
-    public bool userRegistered(User genericUser)
+    public bool userRegistered(User userToBeLogged)
     {
-        throw new NotImplementedException();
+        foreach (var account in _database.Users)
+        {
+            if (account.Email.Equals(account.Email))
+
+            {
+                if (account.Password.Equals(account.Password))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
+    
+    
+    
+    
+    
+    
+    
 }
