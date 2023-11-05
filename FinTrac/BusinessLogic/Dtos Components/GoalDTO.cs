30d7ed431f7e837e026e58525639510f1e788c73
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2023-11-05 02:26:32 -0300

    [RED] Given title, should be setted

---
+++ FinTrac/BusinessLogic/Dtos Components/GoalDTO.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLogic.Category_Components;
+using BusinessLogic.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Dtos_Components
+{
+    public class GoalDTO
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ FinTrac/ProjectTests/Dtos Tests/GoalDTO_Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic.Dto_Components;
+using BusinessLogic.Dtos_Components;
+
+namespace BusinessLogicTests.Dtos_Tests
+{
+    [TestClass]
+    public class GoalDTO_Tests
+    {
+        private GoalDTO _goalDTO;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _goalDTO = new GoalDTO();
+        }
+        [TestMethod]
+        public void GivenTitle_ShouldBeSetted()
+        {
+            string goalDTOTitle = "Less Party";
+            _goalDTO.Title = goalDTOTitle;
+            Assert.AreEqual(goalDTOTitle, );
+        }
+    }
+}
