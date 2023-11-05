f20a2c2d74e97055a370f9360653571bde759ec1
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2023-11-05 19:44:05 -0300

    [RED] Given currency, should be setted

---
+++ FinTrac/BusinessLogic/Dtos Components/ExchangeHistoryDTO.cs
@@ -0,0 +1,14 @@
+﻿using BusinessLogic.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Dtos_Components
+{
+    public class ExchangeHistoryDTO
+    {
+        public CurrencyEnum Currency { get; set; }
+    }
+}
---
+++ FinTrac/ProjectTests/Dtos Tests/ExchangeHistory_Tests.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLogic.Dtos_Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic.Enums;
+
+namespace BusinessLogicTests.Dtos_Tests
+{
+    [TestClass]
+    public class ExchangeHistory_Tests
+    {
+        private ExchangeHistoryDTO _exchangeHistoryDTO;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _exchangeHistoryDTO = new ExchangeHistoryDTO();
+        }
+
+        [TestMethod]
+        public void GivenCurrency_ShouldBeSetted()
+        {
+            _exchangeHistoryDTO.Currency = CurrencyEnum.USA;
+            Assert.AreEqual(false, CurrencyEnum.USA);
+        }
+    }
+}
