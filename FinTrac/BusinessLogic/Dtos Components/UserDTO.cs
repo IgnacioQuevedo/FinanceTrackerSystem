668a3d364f15a3713d80a450616879b2c0b73510
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-11-07 13:43:52 -0300

    [GREEN] Id is setted

--- FinTrac/BusinessLogic/Dtos Components/UserDTO.cs
+++ FinTrac/BusinessLogic/Dtos Components/UserDTO.cs
@@ -4,6 +4,7 @@ namespace BusinessLogic.Dto_Components;
 
 public class UserDTO
 {
+    public int UserId { get; set; }
     public string FirstName { get; set; } = "";
     public string LastName { get; set; } = "";
     public string Email { get; set; } = "";
