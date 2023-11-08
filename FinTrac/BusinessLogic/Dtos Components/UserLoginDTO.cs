69a8ec3f3d71825a12cf0ce755104c441d464275
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2023-11-08 13:58:48 -0300

    [RED] Given email, should be setted

---
+++ FinTrac/BusinessLogic/Dtos Components/UserLoginDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Dtos_Components
+{
+    public class UserLoginDTO
+    {
+        public string Email { get; set; }
+
+    }
+}
---
+++ FinTrac/ProjectTests/Dtos Tests/UserLoginDTOTests.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLogic.Dtos_Components;
+
+namespace BusinessLogicTests.Dto_Components
+{
+    [TestClass]
+    public class UserLoginDTO_Tests
+    {
+        #region Initialize
+        private UserLoginDTO UserLoginDTO;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            UserLoginDTO = new UserLoginDTO();
+        }
+        #endregion
+
+        [TestMethod]
+        public void GivenEmail_ShouldBeSetted()
+        {
+            UserLoginDTO.Email = "someone@example.com";
+            Assert.AreEqual("@example.com", UserLoginDTO.Email);
+        }
+
+    }
+}
