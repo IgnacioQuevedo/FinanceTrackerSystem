198d74084f81bd818fd56b035ef7528c5d4ede48
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2023-11-08 15:18:26 -0300

    [GREEN] Given Id, is setted

--- FinTrac/ProjectTests/Dtos Tests/UserLoginDTOTests.cs
+++ FinTrac/ProjectTests/Dtos Tests/UserLoginDTOTests.cs
@@ -23,7 +23,7 @@ namespace BusinessLogicTests.Dto_Components
         public void GivenId_ShouldBeSetted()
         {
             UserLoginDTO.UserId = 1;
-            Assert.AreEqual(3, UserLoginDTO.UserId);
+            Assert.AreEqual(1, UserLoginDTO.UserId);
         }
 
         #region Email
