dd72f86dc41ab49c40938c83a86c659ddb52f4a7
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-11-08 13:18:06 -0300

    [REFACTOR] Removed ExceptionController

---
+++ FinTrac/Controller/Mappers/ExceptionMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Exceptions
+{
+    public class ExceptionExchangeHistoryManagement : Exception
+    {
+        public ExceptionExchangeHistoryManagement(string message) : base(message) { }
+    }
+}
