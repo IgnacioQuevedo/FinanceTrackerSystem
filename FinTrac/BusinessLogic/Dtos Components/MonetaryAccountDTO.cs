19db8005e3bbb0c6ffec1ba908ab20459cf7210c
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2023-11-11 17:39:14 -0300

    [RED] Given name, should be setted

---
+++ FinTrac/BusinessLogic/Dtos Components/MonetaryAccountDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BusinessLogic.Dtos_Components
+{
+    public class MonetaryAccountDTO
+    {
+        public string Name { get; set; }
+
+        public MonetaryAccountDTO() { }
+    }
+}
---
+++ FinTrac/ProjectTests/Dtos Tests/MonetaryAcccount_Tests.cs
@@ -0,0 +1,16 @@
+using BusinessLogic.Dtos_Components;
+using BusinessLogic.Enums;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class MonetaryAccountDTO_Tests
+    {
+        [TestMethod]
+
+        public void GivenName_ShouldBeSetted()
+        {
+            Assert.AreEqual(0, 1);
+        }
+    }
+}
