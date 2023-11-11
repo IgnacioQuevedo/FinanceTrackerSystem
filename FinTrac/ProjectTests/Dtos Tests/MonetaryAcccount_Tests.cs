7415d162ae03c813a6474cc29fc8b4549557cfc3
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2023-11-11 17:43:11 -0300

    [REFACTOR] Added region and created var to make it more clear

--- FinTrac/ProjectTests/Dtos Tests/MonetaryAcccount_Tests.cs
+++ FinTrac/ProjectTests/Dtos Tests/MonetaryAcccount_Tests.cs
@@ -6,14 +6,20 @@ namespace TestProject1
     [TestClass]
     public class MonetaryAccountDTO_Tests
     {
+        #region Name
+
         [TestMethod]
 
         public void GivenName_ShouldBeSetted()
         {
             MonetaryAccountDTO myMonetaryAccountDTO = new MonetaryAccountDTO();
+            string nameToSet = "Brou";
+
             myMonetaryAccountDTO.Name = "Brou";
 
-            Assert.AreEqual(myMonetaryAccountDTO.Name, "Brou");
+            Assert.AreEqual(myMonetaryAccountDTO.Name, nameToSet);
         }
+
+        #endregion
     }
 }
