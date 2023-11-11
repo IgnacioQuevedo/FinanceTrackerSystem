66320d40ae3238f828f77d62cf1f12b1b93e69d2
Gonzalo Camejo <gonzalocamejo859@gmail.com>
2023-11-11 20:00:31 -0300

    [RED] Given category of credit account DTO, should be setted

---
+++ FinTrac/BusinessLogic/Dtos Components/CreditCardAccountDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BusinessLogic.Dtos_Components
+{
+    public class CreditCardAccountDTO
+    {
+
+        public int CreditCardAccountId { get; set; }
+
+        public CreditCardAccountDTO() { }
+
+
+    }
+}
---
+++ FinTrac/ProjectTests/Dtos Tests/CreditCardAccountDTO_Tests.cs
@@ -0,0 +1,34 @@
+using BusinessLogic.Dtos_Components;
+using BusinessLogic.Enums;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class CreditCardAccountDTO_Tests
+    {
+
+        #region Initialize
+
+        private CreditCardAccountDTO _creditCardAccountDTO;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _creditCardAccountDTO = new CreditCardAccountDTO();
+        }
+
+        #endregion
+
+        [TestMethod]
+        public void GivenCredtiCardAccountId_ShouldBeSetted()
+        {
+            _creditCardAccountDTO.CreditCardAccountId = 1;
+
+            Assert.AreEqual(_creditCardAccountDTO.CreditCardAccountId, 0);
+
+        }
+
+
+
+    }
+}
