98dfbe12d9986caf9da562e3f4473d3aea6b2baa
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-11-12 16:49:26 -0300

    [GREEN] Type is setted

--- FinTrac/BusinessLogic/Dtos Components/TransactionDTO.cs
+++ FinTrac/BusinessLogic/Dtos Components/TransactionDTO.cs
@@ -6,8 +6,5 @@ public class TransactionDTO
     public DateTime CreationDate { get; set; } = DateTime.Now.Date;
     public decimal Amount { get; set; }
     public CurrencyEnumDTO Currency { get; set; }
-    
-    
-    
-    
+    public TypeEnumDTO Type { get; set; }
 }
