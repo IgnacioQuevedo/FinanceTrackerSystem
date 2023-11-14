b3cf7ba886dffd4d387dd6bad4a2ca2e260c2b19
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-11-13 23:56:35 -0300

    [REFACTOR] Initialized for every test accountDTO variable

--- FinTrac/BusinessLogic/Dtos Components/AccountDTO.cs
+++ FinTrac/BusinessLogic/Dtos Components/AccountDTO.cs
@@ -2,5 +2,7 @@ namespace BusinessLogic.Dtos_Components;
 
 public class AccountDTO
 {
+    
     public int AccountId { get; set; }
+    public bool isMonetary { get; set; }
 }
--- FinTrac/ProjectTests/Dtos Tests/AccountDTO_Tests.cs
+++ FinTrac/ProjectTests/Dtos Tests/AccountDTO_Tests.cs
@@ -6,11 +6,14 @@ namespace TestProject1
     [TestClass]
     public class AccountDTO_Tests
     {
+        private AccountDTO accountDTO;
+
         #region Initialize
 
         [TestInitialize]
         public void Initialize()
         {
+            accountDTO = new AccountDTO();
         }
 
         #endregion
@@ -21,24 +24,20 @@ namespace TestProject1
         public void GivenAccountId_ShouldBeSetted()
         {
             int accountId = 1;
-
-            AccountDTO accountDTO = new AccountDTO();
             accountDTO.AccountId = accountId;
 
             Assert.AreEqual(accountDTO.AccountId, accountId);
         }
+
         #endregion
 
         [TestMethod]
         public void GivenBoolThatDeterminesIfTheAccountIsMonetary_ShouldBeSetted()
         {
-
             bool isMonetary = false;
-            AccountDTO accountDTO = new AccountDTO();
             accountDTO.isMonetary = isMonetary;
-            
-            Assert.IsFalse(isMonetary);
 
+            Assert.IsFalse(isMonetary);
         }
     }
 }
