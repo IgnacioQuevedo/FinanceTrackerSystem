ada1e810f5b908a76d1bd86fad0ffb8a906e82fd
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-11-13 23:51:41 -0300

    [REFACTOR] Added region

--- FinTrac/ProjectTests/Dtos Tests/AccountDTO_Tests.cs
+++ FinTrac/ProjectTests/Dtos Tests/AccountDTO_Tests.cs
@@ -6,15 +6,16 @@ namespace TestProject1
     [TestClass]
     public class AccountDTO_Tests
     {
-
         #region Initialize
-        
+
         [TestInitialize]
         public void Initialize()
         {
-       
         }
+
         #endregion
+
+        #region Setting an Id
 
         [TestMethod]
         public void GivenAccountId_ShouldBeSetted()
@@ -23,16 +24,9 @@ namespace TestProject1
 
             AccountDTO accountDTO = new AccountDTO();
             accountDTO.AccountId = accountId;
-            
-            Assert.AreEqual(accountDTO.AccountId,accountId);
-            
-            
-            
-        }
-   
-        
-        
-        
-        
 
+            Assert.AreEqual(accountDTO.AccountId, accountId);
+        }
+
+        #endregion
     }
