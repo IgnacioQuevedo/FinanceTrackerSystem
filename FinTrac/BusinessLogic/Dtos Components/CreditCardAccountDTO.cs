39e92fe6201a7ba38ef8b4b00a33b8f9c9e8771c
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-11-14 00:20:20 -0300

    [REFACTOR] Made CreditCardAccountDTO be a subclass of AccountDTO

--- FinTrac/BusinessLogic/Dtos Components/CreditCardAccountDTO.cs
+++ FinTrac/BusinessLogic/Dtos Components/CreditCardAccountDTO.cs
@@ -8,9 +8,8 @@ using System.Xml.Linq;
 
 namespace BusinessLogic.Dtos_Components
 {
-    public class CreditCardAccountDTO
+    public class CreditCardAccountDTO : AccountDTO
     {
-
         public int CreditCardAccountId { get; set; }
         public string Name { get; set; }
         public CurrencyEnumDTO Currency { get; set; }
@@ -21,9 +20,14 @@ namespace BusinessLogic.Dtos_Components
         public DateTime ClosingDate { get; set; }
         public int? UserId { get; set; }
 
-        public CreditCardAccountDTO() { }
+        public CreditCardAccountDTO()
+        {
+        }
 
-        public CreditCardAccountDTO(string name, CurrencyEnumDTO currency, DateTime creationDate, string issuingBank, string last4digits, decimal availableCredit, DateTime closingDate, int? userId)
+        public CreditCardAccountDTO(string name, CurrencyEnumDTO currency, DateTime creationDate, string issuingBank,
+            string last4digits, decimal availableCredit, DateTime closingDate, int? userId)
+            : base(name, currency, creationDate, userId)
+
         {
             Name = name;
             Currency = currency;
@@ -34,7 +38,5 @@ namespace BusinessLogic.Dtos_Components
             ClosingDate = closingDate;
             UserId = userId;
         }
-
-
     }
 }
