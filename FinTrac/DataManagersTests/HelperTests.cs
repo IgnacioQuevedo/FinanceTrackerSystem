45a5e4e508124cd694c63d828cc381cd6b93d715
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-11-15 20:44:56 -0300

    [RED] Given two equals lists, should return true

--- FinTrac/DataManagersTests/HelperTests.cs
+++ FinTrac/DataManagersTests/HelperTests.cs
@@ -55,7 +55,7 @@ namespace DataManagersTests
             CategoryDTO categoryDto2 = new CategoryDTO("Food", StatusEnumDTO.Enabled, TypeEnumDTO.Income,1);
 
             
-            Assert.IsFalse(Helper.AreTheSameObject(categoryDto.Name,categoryDto2.Name));
+            Assert.IsTrue(Helper.AreTheSameObject(categoryDto.Name,categoryDto2.Name));
             
         }
         
@@ -71,7 +71,23 @@ namespace DataManagersTests
 
         #region Validation Of Two Lists
 
-        
+        [TestMethod]
+        public void GivenTwoListsThatAreEqual_AreTheSameObject_ShouldReturnFalse()
+        {
+            CategoryDTO categoryDto = new CategoryDTO("Food", StatusEnumDTO.Enabled, TypeEnumDTO.Income,1);
+            CategoryDTO categoryDto2 = new CategoryDTO("Fsood", StatusEnumDTO.Enabled, TypeEnumDTO.Income,1);
+
+            
+            List<CategoryDTO> categories = new List<CategoryDTO>();
+            categories.Add(categoryDto2);
+            categories.Add(categoryDto);
+            
+            List<CategoryDTO> categories2 = new List<CategoryDTO>();
+            categories2.Add(categoryDto2);
+            categories2.Add(categoryDto);
+            
+            Assert.IsFalse(Helper.AreTheSameObject(categories,categories2));
+        }
 
         #endregion
 
