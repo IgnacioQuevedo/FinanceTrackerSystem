4eeeaac9c2626144d592ddee19a93c21c1cad513
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-11-15 18:42:57 -0300

    Removed ICategory from GoalCreate page

--- FinTrac/Controller/IControllers/IGoalController.cs
+++ FinTrac/Controller/IControllers/IGoalController.cs
@@ -6,7 +6,8 @@ namespace Controller.IControllers
     {
         public void CreateGoal(GoalDTO dtoToAdd);
         public List<GoalDTO> GetAllGoalsDTO(int userConnectedId);
-
+        public CategoryDTO FindCategory(int idOfCategoryToFind, int idUserConnected);
+        public List<CategoryDTO> GetAllCategories(int userConnectedId);
 
     }
 }
