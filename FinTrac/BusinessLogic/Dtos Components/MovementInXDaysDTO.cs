09978a4796e53e52f7f5dbd410c0ec330abeb5df
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-11-15 00:00:43 -0300

    [RED] Given spendings, should be set to DTO

---
+++ FinTrac/BusinessLogic/Dtos Components/MovementInXDaysDTO.cs
@@ -0,0 +1,11 @@
+
+namespace BusinessLogic.Dtos_Components
+{
+	public class MovementInXDaysDTO
+	{
+		public int[] Spendings { get; set; }
+		
+		
+
+	}
+}
---
+++ FinTrac/ProjectTests/Dtos Tests/MovementInXDays_Tests.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLogic.Dtos_Components;
+
+namespace BusinessLogicTests.Dto_Components
+{
+    [TestClass]
+    public class MovementInXDays_Tests
+    {
+        #region Initialize
+        private UserLoginDTO UserLoginDTO;
+        private string genericEmail;
+        private string genericPassword;
+        private int genericId;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            UserLoginDTO = new UserLoginDTO();
+            genericEmail = "someone@example.com";
+            genericPassword = "ABCDE12345678";
+            genericId = 1;
+        }
+        #endregion
+
+        [TestMethod]
+        public void GivenSpendingArray_ShouldBeSet()
+        {
+            MovementInXDaysDTO movements = new MovementInXDaysDTO();
+            movements.Spendings = spendings;
+            
+            Assert.AreEqual(spendings,movements.Spendings);
+
+        }
+
+
+    }
+}
