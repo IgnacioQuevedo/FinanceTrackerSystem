bfb93077a0a072fe3f977e6943161fdef50d1a4f
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-11-15 15:00:12 -0300

    Removed updation of currency in BL transaction method

--- FinTrac/BusinessLogic/Account Components/Account.cs
+++ FinTrac/BusinessLogic/Account Components/Account.cs
@@ -88,7 +88,6 @@ namespace BusinessLogic.Account_Components
                 if (HaveSameId(transactionToUpdate, i))
                 {
                     MyTransactions[i].Amount = transactionToUpdate.Amount;
-                    MyTransactions[i].Currency = transactionToUpdate.Currency;
                     MyTransactions[i].TransactionCategory = transactionToUpdate.TransactionCategory;
                     flag = true;
                     
