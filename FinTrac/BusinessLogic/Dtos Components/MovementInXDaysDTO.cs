f2238e0fbeb553a6109c4b5bd39bb9f7390454b5
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-11-15 00:22:33 -0300

    Removed blank spaces

--- FinTrac/BusinessLogic/Dtos Components/MovementInXDaysDTO.cs
+++ FinTrac/BusinessLogic/Dtos Components/MovementInXDaysDTO.cs
@@ -1,9 +1,12 @@
 
+using BusinessLogic.Report_Components;
+
 namespace BusinessLogic.Dtos_Components
 {
 	public class MovementInXDaysDTO
 	{
 		public decimal[] Spendings { get; set; }
 		public decimal[] Income { get; set; }
+		
 	}
 }
